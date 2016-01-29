9fd3c715c8502589ec32d34888c513a6697b15d2
David Kirkland <davidrkirkland@gmail.com>
2016-01-30 00:32:02 +0900

    Fixed NextDouble method on RandomBase. Translated RandomBase XML comments to English. Added extension methods for Next() random functionality (including 2 overloads).

---
+++ src/Rei.Random.Tests/TestGeneratedResults.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Rei.Random.Tests
+{
+    
+    public class TestGeneratedResults
+    {
+        [Fact]
+        public void ensure_NextDouble_returns_a_value_between_0_and_1()
+        {
+            var random = new MersenneTwister();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Assert.InRange(random.NextDouble(), 0.0, 1.0);
+            }
+        }
+    }
+}
---
+++ src/Rei.Random/RandomExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Rei.Random
+{
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a non-negative random integer.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        public static int Next(this RandomBase random)
+        {
+            return Math.Abs(random.NextInt32());
+        }
+
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. maxValue must be greater than or equal to 0.</param>
+        /// <returns></returns>
+        public static int Next(this RandomBase random, int maxValue)
+        {
+            if (maxValue < 0)
+            {
+                throw new ArgumentOutOfRangeException("maxValue", "maxValue is less than 0.");
+            }
+            return (int) (random.NextDouble()*maxValue);
+        }
+
+        /// <summary>
+        /// Returns a random integer that is within a specified range.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. maxValue must be greater than or equal to minValue.</param>
+        /// <returns></returns>
+        public static int Next(this RandomBase random, int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                throw new ArgumentOutOfRangeException("minValue", "minValue is greater than maxValue.");
+            }
+            return minValue + (int) (random.NextDouble()*(maxValue - minValue));
+        }
+    }
+}
